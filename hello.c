fd3abee714a92ef36d6cd4e0cc5c985600b92617
sharonchuangmeta <118582906+sharonchuangmeta@users.noreply.github.com>
2022-11-19 16:31:15 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -32,3 +32,24 @@ int main(){
      printf("A/B=%d\n", a/b);
      printf("A %% B=%d\n", a%b);
      return 0;
+
+    
+    #include <stdio.h>
+using namespace std;
+int main(void)
+{
+   int a=9,b=6;
+   printf("first number:\n");
+   scanf("%d",&a);
+   printf("second number:\n");
+   scanf("%d",&b);
+   if(a>b){
+        printf("a>b");
+   }else if (a<b){
+       printf("a<b");
+   }else{
+       printf("a=b");
+   }
+   return 0;
+}
+    
