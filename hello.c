6f2239ccc8088471fd0f6cbd9238bf03e0452c36
sharonchuangmeta <118582906+sharonchuangmeta@users.noreply.github.com>
2022-11-19 13:56:12 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -6,14 +6,29 @@ int main()
 
     return 0;
 }
+
 #include <stdio.h>
-int main(void){
+int main(){
 printf("Hello Selena");
-printf("my height is %d m %d cm \n",1,80); 
-printf("this building is %5d cm \n",890); 
+printf("my height is %d m %d cm \n",1,80);
+printf("this building is %5d cm \n",890);
 printf("this building is %5d cm \n",7890);
 printf("this building is %5d cm \n",6890);
 printf("this building is %5d cm \n",567890);
 printf("this building is %5d cm \n",1234567890);
 printf("this building is %5d cm \n",1234567890);
 printf("pi = %2.5f \n",3.1415926535897932384626433832795);  
+    
+    #include <stdio.h>
+int main(){
+     int a, b;
+     printf("A=> ");
+     scanf("%d", &a);
+     printf("B=> ");
+     scanf("%d", &b);
+     printf("A+B=%d\n", a+b);
+     printf("A-B=%d\n", A-b);
+     printf("A*B=%d\n", a*b);
+     printf("A/B=%d\n", a/b);
+     printf("A % B=%d\n", a % b);
+     return 0;
